e9c8d76767fec0fb2702f9815812e3e75d5e0919
Alexander Niklasson <alexander.niklasson@live.com>
2024-02-05 11:26:28 +0100

    extension part 1 - fixed duplicate directors and writer

--- db/migrations/V13_0_0__Add_Data_To_Peoples_Table.sql
+++ db/migrations/V13_0_0__Add_Data_To_Peoples_Table.sql
@@ -1,13 +1,14 @@
-INSERT INTO people (name, email, birthDate, role, country) VALUES
+INSERT INTO people (name, email, birthDate, role, country)
+VALUES
     ('Stanley Kubrick', NULL, NULL, 'director', 'USA'),
-    ('George Lucas', NULL, NULL, 'director', 'USA'),
+    ('George Lucas', 'george@email.com', NULL, 'director', 'USA'),
     ('Robert Mulligan', NULL, NULL, 'director', 'USA'),
-    ('James Cameron', NULL, NULL, 'director', 'Canada'),
+    ('James Cameron', 'james@cameron.com', NULL, 'director', 'Canada'),
     ('David Lean', NULL, NULL, 'director', 'UK'),
     ('Anthony Mann', NULL, NULL, 'director', 'USA'),
-    ('Theodoros Angelopoulos', NULL, NULL, 'director', 'Greece'),
+    ('Theodoros Angelopoulos', 'theo@angelopoulos.com', NULL, 'director', 'Greece'),
     ('Paul Verhoeven', NULL, NULL, 'director', 'Netherlands'),
-    ('Krzysztof Kieslowski', NULL, NULL, 'director', 'Poland'),
+    ('Krzysztof Kieslowski', 'email@email.com', NULL, 'director', 'Poland'),
     ('Jean-Paul Rappeneau', NULL, NULL, 'director', 'France'),
     ('Keir Dullea', NULL, '30/05/1936', 'star', NULL),
     ('Mark Hamill', NULL, '25/09/1951', 'star', NULL),
@@ -20,12 +21,13 @@ INSERT INTO people (name, email, birthDate, role, country) VALUES
     ('Juliette Binoche', NULL, '09/03/1964', 'star', NULL),
     ('Gerard Depardieu', NULL, '27/12/1948', 'star', NULL),
     ('Arthur C Clarke', 'arthur@clarke.com', NULL, 'writer', NULL),
-    ('George Lucas', 'george@email.com', NULL, 'writer', NULL),
     ('Harper Lee', 'harper@lee.com', NULL, 'writer', NULL),
-    ('James Cameron', 'james@cameron.com', NULL, 'writer', NULL),
     ('Boris Pasternak', 'boris@boris.com', NULL, 'writer', NULL),
     ('Frederick Frank', 'fred@frank.com', NULL, 'writer', NULL),
-    ('Theodoros Angelopoulos', 'theo@angelopoulos.com', NULL, 'writer', NULL),
     ('Erik Hazelhoff Roelfzema', 'erik@roelfzema.com', NULL, 'writer', NULL),
-    ('Krzysztof Kieslowski', 'email@email.com', NULL, 'writer', NULL),
-    ('Edmond Rostand', 'edmond@rostand.com', NULL, 'writer', NULL);
+    ('Edmond Rostand', 'edmond@rostand.com', NULL, 'writer', NULL)
+ON CONFLICT (name) DO UPDATE
+    SET email = EXCLUDED.email,
+        birthDate = EXCLUDED.birthDate,
+        role = EXCLUDED.role,
+        country = EXCLUDED.country;
--- db/migrations/V14_0_0__Truncate_Data_Add_New_Data_Movies_Table.sql
+++ db/migrations/V14_0_0__Truncate_Data_Add_New_Data_Movies_Table.sql
@@ -2,12 +2,12 @@ TRUNCATE TABLE movies;
 
 INSERT INTO movies (title, year, genre, score, directorid, starid, writerid) VALUES
     ('2001: A Space Odyssey', '1968', 'Science Fiction', 10, 1, 11, 21),
-    ('Star Wars: A New Hope', '1977', 'Science Fiction', 7, 2, 12, 22),
+    ('Star Wars: A New Hope', '1977', 'Science Fiction', 7, 2, 12, 2),
     ('To Kill A Mockingbird', '1962', 'Drama', 10, 3, 13, 23),
-    ('Titanic', '1997', 'Romance', 5, 4, 14, 24),
+    ('Titanic', '1997', 'Romance', 5, 4, 14, 4),
     ('Dr Zhivago', '1965', 'Historical', 8, 5, 15, 25),
     ('El Cid', '1961', 'Historical', 6, 6, 16, 26),
-    ('Voyage to Cythera', '1984', 'Drama', 8, 7, 17, 27),
+    ('Voyage to Cythera', '1984', 'Drama', 8, 7, 17, 7),
     ('Soldier of Orange', '1977', 'Thriller', 8, 8, 18, 28),
-    ('Three Colours: Blue', '1993', 'Drama', 8, 9, 19, 29),
+    ('Three Colours: Blue', '1993', 'Drama', 8, 9, 19, 9),
     ('Cyrano de Bergerac', '1990', 'Historical', 9, 10, 20, 30);
