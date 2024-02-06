07fa5a142997f54b1b62d412ca4245579d503c34
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-02-06 15:52:06 +0100

    Updated insertion of data into Person table

--- db/migrations/V6_1_0__insert_data_into_person.sql
+++ db/migrations/V6_1_0__insert_data_into_person.sql
@@ -1,38 +1,11 @@
-INSERT INTO Person (name, country)
-VALUES
-('Stanley Kubrick', 'USA'),
-   ('George Lucas', 'USA'),
-   ('Robert Mulligan', 'USA'),
-   ('James Cameron', 'Canada'),
-   ('David Lean', 'UK'),
-   ('Anthony Mann', 'USA'),
-   ('Theodoros Angelopoulos', 'Greece'),
-   ('Paul Verhoeven', 'Netherlands'),
-   ('Krzysztof Kieslowski', 'Poland'),
-   ('Jean-Paul Rappeneau', 'France');
+SELECT name, country
+FROM Directors
+INSERT INTO Person (name, country);
 
-   INSERT INTO Person (name, dob)
-   VALUES
-  ('Keir Dullea', '30/05/1936'),
-  ('Mark Hamill', '25/09/1951'),
-  ('Gregory Peck', '05/04/1916'),
-  ('Leonardo DiCaprio', '11/11/1974'),
-  ('Julie Christie', '14/04/1940'),
-  ('Charlton Heston', '04/10/1923'),
-  ('Manos Katrakis', '14/08/1908'),
-  ('Rutger Hauer', '23/01/1944'),
-  ('Juliette Binoche', '09/03/1964'),
-  ('Gerard Depardieu', '27/12/1948');
+SELECT name, dob
+FROM Stars
+INSERT INTO Person (name, dob);
 
-  INSERT INTO Person (name, email)
-  VALUES
-  ('Arthur C Clarke', 'arthur@clarke.com'),
-    ('George Lucas', 'george@email.com'),
-    ('Harper Lee', 'harper@lee.com'),
-    ('James Cameron', 'james@cameron.com'),
-    ('Boris Pasternak', 'boris@boris.com'),
-    ('Frederick Frank', 'fred@frank.com'),
-    ('Theodoros Angelopoulos', 'theo@angelopoulos.com'),
-    ('Erik Hazelhoff Roelfzema', 'erik@roelfzema.com'),
-    ('Krzysztof Kieslowski', 'email@email.com'),
-    ('Edmond Rostand', 'edmond@rostand.com');
+SELECT name, email
+FROM Writers
+INSERT INTO Person (name, email);
