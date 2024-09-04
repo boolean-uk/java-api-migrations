c98e1a42a4745c2b5b9a63440d0a996b280291e5
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-04 11:32:16 +0200

    Deleted superfluous columns from initial table.

---
+++ db/migrations/V2_1_0__Delete_superfluous_rows_from_initial_table.sql
@@ -0,0 +1,7 @@
+ALTER TABLE films
+DROP COLUMN director_name,
+DROP COLUMN director_country,
+DROP COLUMN star_name,
+DROP COLUMN star_date_of_birth,
+DROP COLUMN writer_name,
+DROP COLUMN writer_email;
