385946a809e3e940c36557e3d79bd8115dad1904
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-05 10:55:09 +0200

    EXTENSION: Updated create table statement to include id, made name unique to avoid duplication, changed existing constraints in films table.

--- db/migrations/V4_0_0__Create_people_table.sql
+++ db/migrations/V4_0_0__Create_people_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS persons (
+    id SERIAL PRIMARY KEY,
     name TEXT,
     country TEXT,
     dob DATE,
-    email TEXT
+    email TEXT,
+    CONSTRAINT unique_name UNIQUE(name)
 );
-
--- db/migrations/V4_1_0__Populate_persons_table.sql
+++ db/migrations/V4_1_0__Populate_persons_table.sql
@@ -1,8 +1,22 @@
+-- Insert values into persons, only if they do not exist already.
 INSERT INTO persons(name, country)
-    SELECT name, country FROM directors;
+    SELECT name, country
+        FROM directors
+            ON CONFLICT (name)
+                DO UPDATE
+                    SET country = COALESCE(persons.country, EXCLUDED.country);
 
 INSERT INTO persons(name, dob)
-    SELECT name, dob FROM stars;
+    SELECT name, dob
+        FROM stars
+            ON CONFLICT (name)
+                DO UPDATE
+                    SET dob = COALESCE(persons.dob, EXCLUDED.dob);
 
 INSERT INTO persons(name, email)
-    SELECT name, email FROM writers;
+    SELECT name, email
+        FROM writers
+            ON CONFLICT (name)
+                DO UPDATE
+                    SET email = COALESCE(persons.email, EXCLUDED.email);
+
