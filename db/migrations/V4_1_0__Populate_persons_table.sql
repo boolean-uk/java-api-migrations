7264c3bc56c72f4ae9f57d877844791a08fce857
Thomas Wiik <thomas.wiik@no.experis.com>
2024-09-05 11:28:14 +0200

    EXTENSION: Added extra actors to persons table.

--- db/migrations/V4_1_0__Populate_persons_table.sql
+++ db/migrations/V4_1_0__Populate_persons_table.sql
@@ -20,3 +20,15 @@ INSERT INTO persons(name, email)
                 DO UPDATE
                     SET email = COALESCE(persons.email, EXCLUDED.email);
 
+-- New actors for cast
+INSERT INTO persons (person_name, person_email, person_country, person_dob) VALUES
+('Gary Lockwood', 'gary.lockwood@example.com', 'USA', '1937-02-21'),
+('William Sylvester', 'william.sylvester@example.com', 'USA', '1922-01-31'),
+('Harrison Ford', 'harrison.ford@example.com', 'USA', '1942-07-13'),
+('Carrie Fisher', 'carrie.fisher@example.com', 'USA', '1956-10-21'),
+('John Megna', 'john.megna@example.com', 'USA', '1952-11-04'),
+('Brock Peters', 'brock.peters@example.com', 'USA', '1927-07-02'),
+('Kate Winslet', 'kate.winslet@example.com', 'UK', '1975-10-05'),
+('Billy Zane', 'billy.zane@example.com', 'USA', '1966-02-24'),
+('Omar Sharif', 'omar.sharif@example.com', 'Egypt', '1932-04-10'),
+('Geraldine Chaplin', 'geraldine.chaplin@example.com', 'USA', '1944-07-31');
