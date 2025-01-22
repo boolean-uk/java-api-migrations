d9dd4146b3ce5b8215329a6d3bbbad424e708cdf
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-22 14:16:51 +0100

    Move core files to migrations_core

---
+++ db/migrations_core/V5_0_0__create_genres_table.sql
@@ -0,0 +1,4 @@
+ CREATE TABLE IF NOT EXISTS Genres (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
---
+++ db/migrations_core/V6_2_0__genres_inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO genres
+(id, name)
+VALUES
+    (1, 'Science Fiction'),
+    (2, 'Drama'),
+    (3, 'Romance'),
+    (4, 'Historical'),
+    (5, 'Thriller')
