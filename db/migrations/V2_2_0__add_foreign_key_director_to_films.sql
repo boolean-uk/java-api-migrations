2f4cab21175533275391b7fd4c23f5583356a3ad
Thomas Kristiansen <thomaskri0801@hotmail.com>
2025-01-22 14:40:26 +0100

    Core done

---
+++ db/migrations/V2_2_0__add_foreign_key_director_to_films.sql
@@ -0,0 +1,6 @@
+ALTER TABLE films
+ADD COLUMN director_id INT;
+
+ALTER TABLE films ADD CONSTRAINT fk_director_id FOREIGN KEY (director_id) REFERENCES directors (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
